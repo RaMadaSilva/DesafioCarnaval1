0626fc524bc2ab9341744071a9e2bb6574203c0e
RaMadaSilva <raul01mateia@gmail.com>
2023-02-17 17:47:49 +0100

    desafio1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+namespace desafio1;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Escreva o que quiseres: ");
+        var text = Console.ReadLine();
+
+        Console.Write($"> {ContarPalavras(text)} palavras, {ContadorLetras(text)} caracteres");
+    }
+
+    static int ContarPalavras(string texto)
+    {
+        char[] delimitador = { ',', ' ', '.', ':' };
+        var palavras = texto.Replace(" ", "").TrimEnd().Split(delimitador).ToList();
+        foreach (var lt in palavras)
+            Console.WriteLine(lt);
+        return palavras.Count();
+    }
+
+    static int ContadorLetras(string texto)
+    {
+        var letra = texto.Replace(" ", "").Replace(",", "").Replace(".", "");
+        return letra.Count();
+    }
+
+}
